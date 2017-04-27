199427773a42d9873002a1dd5427ab28f8bde8ae
romewing <evechuang@qq.com>
2017-04-27 23:43:43 +0800

    Initial commit

---
+++ webmagic-spring-boot-samples/src/main/java/org/github/spring/boot/samples/WebmagicApplication.java
@@ -0,0 +1,14 @@
+package org.github.spring.boot.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2017/4/26.
+ */
+@SpringBootApplication
+public class WebmagicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebmagicApplication.class);
+    }
+}
